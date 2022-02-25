02b174f3f64eb01ff7c16f9c7646152dd6194d2a
Theo Gainey <theo@heysparkbox.com>
2022-02-25 12:38:57 -0500

    class based inheritance solution

--- src/rogue/index.js
+++ src/rogue/index.js
@@ -1,10 +1,23 @@
 import Character from '../character';
 
-class Rogue {
+class Rogue extends Character{
+  constructor(name){
+    super(name);
+    this.attackCount =0;
+  }
   talk() {
+    return super.talk().toLowerCase().replace('!', '.');
   }
 
   attack() {
+    this.attackCount += 1;
+    if(this.attackCount % 5 !== 0){
+      return 5;
+    }
+    else {
+      this.health -=4;
+      return super.attack();
+    }
   }
 }
 
--- src/wizard/index.js
+++ src/wizard/index.js
@@ -1,10 +1,19 @@
 import Character from '../character';
 
-class Wizard {
+class Wizard extends Character{
   talk() {
+    return super.talk().toUpperCase();
   }
 
   attack() {
+    if(typeof this.protectionSpell === 'undefined') this.protectionSpell = 3;
+    if(this.protectionSpell > 0){
+      this.protectionSpell -= 1;
+      this.health +=1;
+      return 2* super.attack();
+    }
+    this.health -=9;
+    return super.attack();
   }
 }
 
